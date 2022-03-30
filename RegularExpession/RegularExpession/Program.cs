db7d3c2d3e798a1d6100f54fd0d8440b18ea07c3
pappurathod <prathod4545@gmail.com>
2022-03-30 22:22:33 +0530

    Last name validation

--- RegularExpession/RegularExpession/Program.cs
+++ RegularExpession/RegularExpession/Program.cs
@@ -6,9 +6,13 @@ namespace RegularExpession
         public static void Main(string[] args)
         {
             UserRegistration userRegistration = new UserRegistration();
-            Console.WriteLine("Enter first name");
+            Console.WriteLine("Enter First name");
             string FirstName=Console.ReadLine();
             userRegistration.validFirstName(FirstName);
+
+            Console.WriteLine("Enter Last name");
+            string LastName = Console.ReadLine();
+            userRegistration.validLastName(LastName);
         }        
     }
 }
--- RegularExpession/RegularExpession/UserRegistration.cs
+++ RegularExpession/RegularExpession/UserRegistration.cs
@@ -10,6 +10,7 @@ namespace RegularExpession
     public class UserRegistration
     {
         public Regex FirstNameR =new Regex(@"^[A-Z]{1}[a-zA-Z]{2,}$");
+        public Regex LastNameR = new Regex(@"^[A-Z]{1}[a-zA-Z]{2,}$");
         public void validFirstName(string FirstName)
         {
             
@@ -20,6 +21,18 @@ namespace RegularExpession
             else
             {
                 Console.WriteLine("{0} First Name is not valid", FirstName);
+            }
+        }
+        public void validLastName(string LastName)
+        {
+
+            if (FirstNameR.IsMatch(LastName))
+            {
+                Console.WriteLine("{0} Last Name is valid", LastName);
+            }
+            else
+            {
+                Console.WriteLine("{0} Last Name is not valid", LastName);
             }
         }
     }
