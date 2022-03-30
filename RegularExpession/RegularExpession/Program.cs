e92199a07ed6871f3de9badf7acf9051f4a952fa
pappurathod <prathod4545@gmail.com>
2022-03-30 22:12:58 +0530

    First name validation

---
+++ RegularExpession/RegularExpession/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RegularExpession
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            Console.WriteLine("Enter first name");
+            string FirstName=Console.ReadLine();
+            userRegistration.validFirstName(FirstName);
+        }        
+    }
+}
---
+++ RegularExpession/RegularExpession/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularExpession
+{
+    public class UserRegistration
+    {
+        public Regex FirstNameR =new Regex(@"^[A-Z]{1}[a-zA-Z]{2,}$");
+        public void validFirstName(string FirstName)
+        {
+            
+            if (FirstNameR.IsMatch(FirstName))
+            {
+                Console.WriteLine("{0} First Name is valid",FirstName);
+            }
+            else
+            {
+                Console.WriteLine("{0} First Name is not valid", FirstName);
+            }
+        }
+    }
+}
