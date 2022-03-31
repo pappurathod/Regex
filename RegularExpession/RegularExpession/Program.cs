46b9a4b100a5a29df24a8ec079667e9c8fdf7422
pappurathod <prathod4545@gmail.com>
2022-03-31 11:49:25 +0530

    Password rule 1 minimum 8 char

--- RegularExpession/RegularExpession/Program.cs
+++ RegularExpession/RegularExpession/Program.cs
@@ -6,23 +6,53 @@ namespace RegularExpession
         public static void Main(string[] args)
         {
             UserRegistration userRegistration = new UserRegistration();
-            Console.WriteLine("Enter First name");
-            string FirstName = Console.ReadLine();
-            userRegistration.validFirstName(FirstName);
+            int choice;
+            do
+            {
+                Console.WriteLine("select your choice");
+                Console.WriteLine("1 for FirstName");
+                Console.WriteLine("2 for LaststName");
+                Console.WriteLine("3 for Email");
+                Console.WriteLine("4 for MobileNumber");
+                Console.WriteLine("5 for Password");
+                Console.WriteLine("6 for Exit");
+                choice = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Last name");
-            string LastName = Console.ReadLine();
-            userRegistration.validLastName(LastName);
-
-            Console.WriteLine("Enter Email");
-            string Email = Console.ReadLine();
-            userRegistration.validEmail(Email);
-
-
-            Console.WriteLine("Enter Mobile Number");
-            string MobileNo = Console.ReadLine();
-            userRegistration.validMobileNo(MobileNo);
-
-        }
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First name");
+                        string fName = Console.ReadLine();
+                        userRegistration.validFirstName(fName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Last name");
+                        string LastName = Console.ReadLine();
+                        userRegistration.validLastName(LastName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Email");
+                        string Email = Console.ReadLine();
+                        userRegistration.validEmail(Email);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Mobile Number");
+                        string MobileNo = Console.ReadLine();
+                        userRegistration.validMobileNo(MobileNo);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Password");
+                        string Password = Console.ReadLine();
+                        userRegistration.validPassword(Password);
+                        break;
+                    case 10:
+                        Console.WriteLine("Thank you");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            } while (choice != 6);
+        }    
     }
 }
--- RegularExpession/RegularExpession/UserRegistration.cs
+++ RegularExpession/RegularExpession/UserRegistration.cs
@@ -13,6 +13,7 @@ namespace RegularExpession
         public Regex LastNameR = new Regex(@"^[A-Z]{1}[a-zA-Z]{2,}$");
         public Regex EmailR = new Regex(@"^[a-z]{3,}([.]{1}[a-z]{3,})?@bl.co([.]{1}[a-z]{2})?$");
         public Regex MobileNoR = new Regex(@"^[0-9]{2}[ ][6-9]{1}[0-9]{9}$");
+        public Regex PasswordR = new Regex(@"^[a-zA-Z0-9]{8,}$");
 
         public void validFirstName(string FirstName)
         {
@@ -57,6 +58,19 @@ namespace RegularExpession
             else
             {
                 Console.WriteLine("{0} Mobile Number is not valid", MobileNo);
+            }
+        }
+
+        public void validPassword(string Password)
+        {
+
+            if (PasswordR.IsMatch(Password))
+            {
+                Console.WriteLine("{0} Password is valid", Password);
+            }
+            else
+            {
+                Console.WriteLine("{0} Password is not valid", Password);
             }
         }
     }
